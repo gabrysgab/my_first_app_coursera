d09ba073b6b831a711dfbca5f0514aa703279caa
Mateusz <mateuszgabrynowicz@gmail.com>
2016-07-25 20:52:43 +0200

    heroku

---
+++ app/controllers/greeter_controller.rb
@@ -0,0 +1,13 @@
+class GreeterController < ApplicationController
+  def hello
+  	random_names = ["Franek", "Bolek", "Lolek"]
+  	@name = random_names.sample
+  	@time = Time.now
+  	@times_displayed ||=0
+  	@times_displayed += 1
+
+  end
+
+  def goodbye
+  end
+end
